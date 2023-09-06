1f58483845d368d4b1c30834182bc67b630f7e38
Vagner Bruno <vagnerbackend@gmail.com>
2023-09-06 17:52:00 -0300

    CRIEI A CLASSE QUE UNE TODOS OS TESTES

---
+++ test/br/com/vbruno/AllTests.java
@@ -0,0 +1,9 @@
+package br.com.vbruno;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ClienteDAOTest.class, ProdutoDAOTest.class})
+public class AllTests {
+}
